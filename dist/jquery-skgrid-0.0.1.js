ec3ddcb70e79a09c049d4630afabed29b550528e
sergey <schernoudov@yandex.ru>
2015-03-10 02:55:49 +0300

    Rendering labels

--- dist/jquery-skgrid-0.0.1.js
+++ dist/jquery-skgrid-0.0.1.js
@@ -1,3 +1,7 @@
+(function ($, window, undefined) {
+    /*jshint validthis: true */
+    "use strict";
+
 var namespace = ".sk.jquery.skgrid";
 
 var init = function(options) {
@@ -61,3 +65,55 @@ var renderLabels = function() {
     labelHolder.css('top', this.element.children('thead').height());
     labelHolder.insertBefore(this.element.parent());
 };
+var SKGrid = function(element, options) {
+	this.element = $(element);
+	this.origin = this.element.clone();
+	this.options = $.extend(true, {}, SKGrid.defaults, this.element.data(), options);
+};
+
+SKGrid.defaults = {
+    requestType: "POST",
+    url: "",
+    renderHeader: false,
+    renderLabels: false,
+    rowClassDeterminator: undefined,
+    cellClassDeterminator: undefined,
+    labels: []
+};
+
+SKGrid.prototype.appendRows = function(rows) {
+};
+
+SKGrid.prototype.loadData = function() {
+    var self = this;
+    $.ajax({
+        url: this.options.url,
+        type: this.options.requestType,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success:    function (data) {
+                        renderTable.call(self, data);
+                    }
+    });
+};
+$.fn.skgrid = function(option) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return this.each(function () {
+        var $this = $(this);
+        var instance = $this.data(namespace);
+        var options = typeof option === "object" && option;
+        if (!instance && option === "destroy") {
+            return;
+        }
+        if (!instance) {
+            $this.data(namespace, (instance = new SKGrid(this, options)));
+            init.call(instance);
+        }
+        if (typeof option === "string") {
+            return instance[option].apply(instance, args);
+        }
+    });
+};
+
+$.fn.skgrid.Constructor = SKGrid;
+})(jQuery, window);
--- src/jquery.skgrid.js
+++ src/jquery.skgrid.js
@@ -8,20 +8,24 @@ SKGrid.defaults = {
     requestType: "POST",
     url: "",
     renderHeader: false,
-    renderLabels: false
+    renderLabels: false,
+    rowClassDeterminator: undefined,
+    cellClassDeterminator: undefined,
+    labels: []
 };
 
 SKGrid.prototype.appendRows = function(rows) {
 };
 
 SKGrid.prototype.loadData = function() {
+    var self = this;
     $.ajax({
         url: this.options.url,
         type: this.options.requestType,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success:    function (data) {
-                        renderTable.call(this, data);
+                        renderTable.call(self, data);
                     }
     });
 };
